da6c70f301be7897fe352bb8522905292afedb59
Nahuel <nahulisas@gmail.com>
2024-10-29 18:36:09 -0300

    se agregan estilos al itemdetail

--- src/componentes/pages/ItemDetail/ItemDetail.jsx
+++ src/componentes/pages/ItemDetail/ItemDetail.jsx
@@ -1,5 +1,13 @@
 const ItemDetail = ({ item }) => {
-    return <div>producto de id {item.id}</div>;
+    return (
+        <>
+            <div className="item__detail__container">
+                <img src={item.imageUrl} alt="" />
+                <h3>{item.title}</h3>
+                <h4>${item.price}</h4>
+            </div>
+        </>
+    );
 };
 
 export default ItemDetail;
